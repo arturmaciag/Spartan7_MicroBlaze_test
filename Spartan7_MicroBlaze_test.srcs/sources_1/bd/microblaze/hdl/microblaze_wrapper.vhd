c996c36d2f0b0ee7ed2318161b5e816e93d23849
amaciag <arturmaciag@o2.pl>
2021-11-23 14:23:22 +0100

    Rebuild project

--- Spartan7_MicroBlaze_test.srcs/sources_1/bd/microblaze/hdl/microblaze_wrapper.vhd
+++ Spartan7_MicroBlaze_test.srcs/sources_1/bd/microblaze/hdl/microblaze_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
---Date        : Mon Nov 22 14:50:15 2021
+--Date        : Tue Nov 23 13:58:55 2021
 --Host        : operator-PC running 64-bit Service Pack 1  (build 7601)
 --Command     : generate_target microblaze_wrapper.bd
 --Design      : microblaze_wrapper
